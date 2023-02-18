7b08d2840b58855918aa0d0b678e99a63d443c63
Filipe Costa <113775030+FilipeAlexCosta@users.noreply.github.com>
2023-02-18 14:16:34 +0000

    [PATCH] Bug fixing and removed global variable
    Fixed a bug where it would not check if the board was filled right after the player made its move, which allowed the computer to play an extra turn on before the game ended.
    Changed the global variable Board and made it into a variable in the scope of main, of course that meant adding extra parameters to functions that altered directly the global variable such as printBoard, resetBoard, playerMove, computerMove, emptySquares and checkWin.
    Changed the scoreboard constant from global to a local variable in the domain of evaluateBoard, since it has no use anywhere else.

--- TictactoeGame.c
+++ TictactoeGame.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
 
-char board[3][3]; // Currently using a global variable, will change that later probably
 const char PLAYER = 'X';
 const char COMPUTER = 'O';
 const char EMPTY_SPACE = '_';
-const int SCORE_BOARD[3][3] = {{3, 2, 3}, {2, 4, 2}, {3, 2, 3}};
 
-void resetBoard();
-void printBoard();
-void playerMove();
+void resetBoard(char board[][3]);
+void printBoard(char board[][3]);
+void playerMove(char board[][3]);
 int evaluateBoard(char board[][3]);
 int moveSearch(char board[][3], int, char);
-void computerMove();
-int emptySquares();
-char checkWin(char);
+void computerMove(char board[][3]);
+int emptySquares(char board[][3]);
+char checkWin(char board[][3], char);
 
 int main()
 {
-    resetBoard(); // Makes sure to initialize the board with EMPTY_SPACE
-    printBoard();
+    char board[3][3];
+    resetBoard(board); // Makes sure to initialize the board with EMPTY_SPACE
+    printBoard(board);
     do {
-        playerMove();
-        if (checkWin(PLAYER) == PLAYER || checkWin(COMPUTER) == COMPUTER)
-        {
-            break; // If one of the players wins, the game ends, otherwise it goes until the board is filled
-        }
-        computerMove();
-        if (checkWin(PLAYER) == PLAYER || checkWin(COMPUTER) == COMPUTER)
-        {
-            break;
-        }
-        printBoard();
-    } while(emptySquares() != 0);
+        playerMove(board);
+        if (emptySquares(board) == 0) break; // if the player starts and it ends in a draw it must not give the PC its turn
+        if (checkWin(board, PLAYER) == PLAYER || checkWin(board, COMPUTER) == COMPUTER) break; // stops if someone won
+        computerMove(board);
+        if (checkWin(board, PLAYER) == PLAYER || checkWin(board, COMPUTER) == COMPUTER) break;
+        printBoard(board);
+    } while(emptySquares(board) != 0);
+    printBoard(board);
     printf("\n");
     return 0;
 }
 
-void resetBoard()
+void resetBoard(char board[][3])
 {
     /* Returns a 3x3 matrix representative of a tictactoe board to its default state
     which is considered to be when it is fully filled with whatever char the constant
@@ -50,7 +45,7 @@ void resetBoard()
     }
 }
 
-void printBoard()
+void printBoard(char board[][3])
 {
     /* Prints a 3x3 matrix that is used to represent the board of a
     tictactoe game */
@@ -64,7 +59,7 @@ void printBoard()
     }
 }
 
-void playerMove()
+void playerMove(char board[][3])
 {
     /* Takes the row and column chosen by the player and, if they are valid,
     sets the value of the board at that position to the value of the const PLAYER */
@@ -75,7 +70,7 @@ void playerMove()
     { // Validates the choice (1 <= row <= 3, 1 <= column <= 3, and unfilled square)
         printf("\nRow: ");
         scanf("%d", &row);
-        printf("\nColumn: ");
+        printf("Column: ");
         scanf("%d", &column);
         printf("\n");
         validChoice = 1;
@@ -103,6 +98,7 @@ int evaluateBoard(char board[][3])
     a square is filled by the computer and adding if its filled by the player.
     Each square has its own value proportional to the number of threes in a row
     passing through it. */
+    const int SCORE_BOARD[3][3] = {{3, 2, 3}, {2, 4, 2}, {3, 2, 3}};
     int score = 0;
     for (int row = 0; row < 3; row++)
     {
@@ -124,7 +120,7 @@ int moveSearch(char board[][3], int depth, char lastPlayer)
     int scoreMinMax = -(WinMultiplier * 100); // Initializes the score in a way it will always be changed
                                               // to the first score calculated
     
-    if (checkWin(lastPlayer) == lastPlayer) return -(WinMultiplier * 42);
+    if (checkWin(board, lastPlayer) == lastPlayer) return -(WinMultiplier * 42);
     if (depth == 0) return evaluateBoard(board);
 
     for (int row = 0; row < 3; row++)
@@ -151,12 +147,12 @@ int moveSearch(char board[][3], int depth, char lastPlayer)
     return scoreMinMax;
 }
 
-void computerMove()
+void computerMove(char board[][3])
 {
     /* Finds out the best possible move the computer can play to either secure its
     win or at the very least a draw */
     int score, scoreMinMax = 100, play[2];
-    int numEmptySquares = emptySquares();
+    int numEmptySquares = emptySquares(board);
 
     if (numEmptySquares == 8 || numEmptySquares == 9)
     { // Either plays on the center or the top left corner as a first move
@@ -179,10 +175,9 @@ void computerMove()
         {
             if (board[row][column] != EMPTY_SPACE) continue;
             board[row][column] = COMPUTER;
-            if (checkWin(COMPUTER) == COMPUTER)
+            if (checkWin(board, COMPUTER) == COMPUTER)
             { // Serves as a way to correct for human unpredictability, so that it takes any
               // chance to win it gets
-                printBoard();
                 return;
             }
             score = moveSearch(board, numEmptySquares - 1, COMPUTER);
@@ -194,7 +189,7 @@ void computerMove()
     return;
 }
 
-int emptySquares()
+int emptySquares(char board[][3])
 {
     /* Gets how many empty squares there are currently on the board.
     Very useful as a stopping condition or for finding out the depth for moveSearch */
@@ -212,7 +207,7 @@ int emptySquares()
     return emptySquares;
 }
 
-char checkWin(char winner)
+char checkWin(char board[][3], char winner)
 {
     /* Checks if there are any three in a rows associated to the winner given by the
     parameter. It will either return the winner if he indeed wins or the const EMPTY_SPACE */
